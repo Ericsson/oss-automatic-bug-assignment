5eaf36a504341259d8acca944424eeae72f381cd
Daniel Artchounin <danar972@student.liu.se>
2017-02-11 12:14:31 +0100

    Updated settings of the Eclipse JDT crawler
    We don't obey robots.txt rules anymore.

--- eclipse_jdt/scrap_eclipse_jdt/scrap_eclipse_jdt/settings.py
+++ eclipse_jdt/scrap_eclipse_jdt/scrap_eclipse_jdt/settings.py
@@ -19,7 +19,7 @@ NEWSPIDER_MODULE = 'scrap_eclipse_jdt.spiders'
 #USER_AGENT = 'scrap_eclipse_jdt (+http://www.yourdomain.com)'
 
 # Obey robots.txt rules
-ROBOTSTXT_OBEY = True
+ROBOTSTXT_OBEY = False
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
 #CONCURRENT_REQUESTS = 32
