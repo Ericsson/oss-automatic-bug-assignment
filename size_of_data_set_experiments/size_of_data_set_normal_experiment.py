95d6ed2e3fa7aa532ea797d071011df5f97911c1
edanart <daniel.artchounin@ericsson.com>
2017-04-25 09:45:42 +0200

    Added the modules related to the preliminary experiment

---
+++ size_of_data_set_experiments/size_of_data_set_normal_experiment.py
@@ -0,0 +1,55 @@
+# -*- coding: utf-8 -*-
+
+from size_of_data_set_experiment import SizeOfDataExperiment
+import numpy as np
+import logging
+        
+class SizeOfDataNormalExperiment(SizeOfDataExperiment):
+    def __init__(self, data_set_file, developers_dict_file, developers_list_file):
+        super().__init__(data_set_file, developers_dict_file, developers_list_file)
+        self._type = "normal"
+        
+    def _yield_indices_for_learning_curve(self, K=33):
+        super()._yield_indices_for_learning_curve(K)
+        number_of_instances = self._X.shape[0]
+        indices = super()._custom_linspace(0, number_of_instances, K+1)
+        beginning_test_set_idx = len(indices)-2 
+        for i in range(beginning_test_set_idx):
+            yield np.asarray(range(indices[i], \
+            indices[beginning_test_set_idx])), \
+            np.asarray(range(indices[beginning_test_set_idx], \
+            indices[beginning_test_set_idx+1]))
+                
+    def _generate_list_indices_for_learning_curve(self, K=33):
+        super()._generate_list_indices_for_learning_curve(K)
+        number_of_instances = self._X.shape[0]
+        indices = super()._custom_linspace(0, number_of_instances, K+1)
+        train_indices = []
+        test_indices = []
+        beginning_test_set_idx = len(indices)-2 
+        for i in range(beginning_test_set_idx):
+            train_indices.append(list(range(indices[i], \
+            indices[beginning_test_set_idx])))
+            test_indices.append(list(range(indices[beginning_test_set_idx], \
+            indices[beginning_test_set_idx+1])))            
+        return train_indices, test_indices 
+        
+    def plot_or_save_learning_curve(self, K=33, save_file=True):
+        super().plot_or_save_learning_curve(K, save_file)
+        
+if __name__ == "__main__":
+    logging.basicConfig(filename="size_of_data_set_normal_experiment.log", \
+    filemode="w", level=logging.DEBUG)
+    data_set_file = "../pre_processing_experiments/output_without_cleaning_without_stemming_without_lemmatizing_without_stop_words_removal_without_punctuation_removal_without_numbers_removal.json" # The path of the file which 
+    # contains the pre-processed output
+    # Below, the path of the file which contains a dictionary related 
+    # to the mappings of the developers
+    developers_dict_file = "../../developers_dict.json" 
+    # Below, the path of the file which contains a list of the 
+    # relevant distinct developers
+    developers_list_file = "../../developers_list.json" 
+    size_of_data_normal_experiment = SizeOfDataNormalExperiment(data_set_file, \
+    developers_dict_file, developers_list_file)
+    
+    K = 4 # Number of folds
+    size_of_data_normal_experiment.plot_or_save_learning_curve(K)
