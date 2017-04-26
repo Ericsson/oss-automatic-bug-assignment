441e50df056abfe307367eac70fd0c5b45e32f4c
Daniel Artchounin <danar972@student.liu.se>
2017-04-26 14:29:41 +0200

    Updated the parent class used in the third experiment
    Based on the different configurations, this parent class is used to run the third experiment accordingly.

--- feature_selection_experiments/classify_k_folds_time_series_feature_selection.py
+++ feature_selection_experiments/classify_k_folds_time_series_feature_selection.py
@@ -19,29 +19,31 @@ current_dir = os.path.dirname(os.path.abspath( \
 inspect.getfile(inspect.currentframe())))
 parent_dir = os.path.dirname(current_dir)
 os.sys.path.insert(0,parent_dir)
-from utilities import print_log, load_data_set, load_developers_mappings, \
-load_distinct_developers_list, build_data_frame
+from utilities import print_log, load_data_set, build_data_frame
 from scikit_learn.rfe import RFECV
 from scikit_learn._search import GridSearchCV
 from scikit_learn.accuracy_mrr_scoring_object import accuracy_mrr_scoring_object
 
 
 class FeatureSelectionExperiment:
-    def __init__(self, data_set_file, developers_dict_file, developers_list_file):
-        self._data_set_file = data_set_file
+    def __init__(self, developers_dict_file=None, \
+                developers_list_file=None):
+        self._current_dir = None
+        self._data_set_file = None
         self._developers_dict_file = developers_dict_file
-        self._developers_list_file = developers_list_file
-        self._current_dir = os.path.dirname(os.path.abspath( \
-        inspect.getfile(inspect.currentframe())))       
-        self.parent_dir = os.path.dirname(self._current_dir)
-        self._tscv = TimeSeriesSplit(n_splits=10)
-        self._train_set = None
-        self._test_set = None
+        self._developers_list_file = developers_list_file   
+        
+        self._tscv = TimeSeriesSplit(n_splits=10) # Used to store a 
+        # reference to the object which will allow us to perform a 
+        # customized version of cross validation
+        
+        self._train_set = None # Used to store a reference to the 
+        # training set
+        self._test_set = None # Used to store a reference to the 
+        # test set
+        
         np.random.seed(0) # We set the seed
-        
-        logging.basicConfig(filename='feature_selection_experiment.log', \
-        filemode='w', level=logging.DEBUG)
-        
+                
         self._pre_processing_steps = [("count", CountVectorizer( \
         lowercase=False, token_pattern=u"(?u)\S+")), \
         ("tf_idf", TfidfTransformer(use_idf=True, smooth_idf=False))]
@@ -66,9 +68,9 @@ class FeatureSelectionExperiment:
             # "LogisticRegression": [("clf", LogisticRegression(random_state=0, class_weight="balanced", multi_class="multinomial", n_jobs=-1))] \
         }
 
-        # Below, there is a dictionary to store the names, the classifiers 
-        # used, the parameters sent to the constructor of the classifiers 
-        # and the fitted classifiers    
+        # Below, there is a dictionary to store the names, the 
+        # classifiers used, the parameters sent to the constructor of
+        # the classifiers and the fitted classifiers    
         self._models_cv = {}
         
         # Below, there is a dictionary to store the names, the pipelines 
@@ -110,18 +112,22 @@ class FeatureSelectionExperiment:
                     "error_score": np.array([-1, -1]), \
                     "scoring": accuracy_mrr_scoring_object
                 }, None]
-        
-        self._build_data_set() 
                 
     def _build_data_set(self):
         # First we load the data of the three aforementioned files
         json_data = load_data_set(self._data_set_file)
-        developers_dict_data = load_developers_mappings(self._developers_dict_file)
-        developers_list_data = load_distinct_developers_list(self._developers_list_file)
+        
+        developers_dict_data = None
+#         TO DO
+#         load_developers_mappings(self._developers_dict_file)
+        developers_list_data = None
+#         TO DO
+#         load_distinct_developers_list(self._developers_list_file)
         
         # Then, we build a data frame using the loaded data set, the 
         # loaded developers mappings, the loaded distinct developers.
-        self._df = build_data_frame(json_data, developers_dict_data, developers_list_data)
+        self._df = build_data_frame(json_data, developers_dict_data, \
+                                    developers_list_data)
         
         print_log("Splitting the data set") # Debug
         # self._df = self._df[-30000:]
@@ -138,6 +144,7 @@ class FeatureSelectionExperiment:
         print_log(self._test_set['class'].value_counts(normalize=True))
         
     def conduct_experiment(self):
+        """Method used to conduct the experiment"""
         self._train_predict_cv()
         self._write_df()
         self._save_cleaned_results()
@@ -149,7 +156,8 @@ class FeatureSelectionExperiment:
         an attribute of the object.
         """
         with open(self._cleaned_results_file_name, 'w') as output_file:
-            json.dump(self._results_to_save_to_a_file, output_file)
+            json.dump(self._results_to_save_to_a_file, output_file, \
+                      indent=4)
         
     def _train_predict_cv(self):
         X_train = self._train_set['text']
@@ -163,7 +171,8 @@ class FeatureSelectionExperiment:
             self._models_cv[key][-1] = self. \
             _models_cv[key][0](**self._models_cv[key][1]) \
             .fit(X_train, y_train)
-            print_log("--- {} seconds ---".format(time.time() - start_time))
+            print_log("--- {} seconds ---" \
+                      .format(time.time() - start_time))
 
         self._results_to_save_to_a_file["avg_accuracy"] = {}
         self._results_to_save_to_a_file["mrr_accuracy"] = {}
@@ -179,9 +188,9 @@ class FeatureSelectionExperiment:
             means[:,0].tolist()
             self._results_to_save_to_a_file["mrr_accuracy"][key] = \
             means[:,1].tolist()
-            print(means)
-            print(stds)
-            print(self._models_cv[key][-1].cv_results_['params'])
+#             print(means)
+#             print(stds)
+#             print(self._models_cv[key][-1].cv_results_['params'])
             for mean, std, params in zip(means, stds, self._models_cv[key][-1].cv_results_['params']):
                 print_log("{} (+/-{}) for {!r}".format(mean, std * 2, params))
             print_log("All results on the training set")
@@ -198,12 +207,13 @@ class FeatureSelectionExperiment:
         print_log("Computation of the weights of the TF-IDF model")
         X_train = tfidf_transformer.fit_transform(X_train_counts)
         
-        print("Training of the models") # Debug
+        print_log("Training of the models") # Debug
         for key in self._rfe_cv:
             start_time = time.time() # We get the time expressed in seconds 
             # since the epoch
             print_log(key)
-            self._rfe_cv[key][-1] = self._rfe_cv[key][0](**self._rfe_cv[key][1]) \
+            self._rfe_cv[key][-1] = \
+            self._rfe_cv[key][0](**self._rfe_cv[key][1]) \
             .fit(X_train, y_train)
             print_log("Number of features")
             print_log(self._rfe_cv[key][-1].n_features_)
@@ -217,29 +227,9 @@ class FeatureSelectionExperiment:
             self._rfe_cv[key][-1].grid_scores_[:,0].tolist()
             self._results_to_save_to_a_file["mrr_accuracy"][key] = \
             self._rfe_cv[key][-1].grid_scores_[:,1].tolist()
-            print("--- {} seconds ---".format(time.time() - start_time)) 
+            print_log("--- {} seconds ---" \
+                      .format(time.time() - start_time)) 
     
     def _write_df(self):
         # We dump the data frame
         self._df.to_csv("pre_processed_data.csv")
-   
-def main():
-    data_set_file = "../pre_processing_experiments/output_with_" + \
-    "cleaning_without_stemming_without_lemmatizing_with_stop_" + \
-    "words_removal_with_punctuation_removal_with_numbers_" + \
-    "removal.json" # The path of the file which contains the 
-    # pre-processed output
-    # Below, the path of the file which contains a dictionary related 
-    # to the mappings of the developers
-    developers_dict_file = "../../developers_dict.json"
-    # Below, the path of the file which contains a list of the 
-    # relevant distinct developers
-    developers_list_file = "../../developers_list.json"
-    
-    feature_selection_experiment = FeatureSelectionExperiment( \
-    data_set_file=data_set_file, developers_dict_file=developers_dict_file, \
-    developers_list_file=developers_list_file)
-    feature_selection_experiment.conduct_experiment()
-    
-if __name__ == "__main__":
-    main()
