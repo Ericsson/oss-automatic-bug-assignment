755022834a2dd7f754c3bf58c8bf4c8dddbfd9b9
Daniel Artchounin <danar972@student.liu.se>
2017-04-26 11:02:07 +0200

    Cleaned the content of the 'DataPreProcesser' class

--- data_pre_processer.py
+++ data_pre_processer.py
@@ -16,137 +16,58 @@ import time
 import os
 import inspect
 
-"""Pre-process BRs
+"""Pre-process bug reports
 
-Pre-process the id, heading, observation and assignee fields of the 
-BRs.
+Pre-process the id, title, description and assigned-to fields of the 
+bug reports.
 """
 
 class DataPreProcesser:
 
     case_sensitive_elements_to_remove = [
-        r"\b\b",
-        r"\b\b",
-        r"\b\b",
-        r"\b\b",
-        r"\b\b",
-        r"\b\b",
-        r"\b\b",
-        r"\b\b",
-        r"\b\b",
-        r"\b\b",
-        r"\b\b",
-        r"\b\b",
-        r"\b\b",
-        r"\b\b",
-        r"\b\b",
-        r"\b\b",
-        r"\b\b",
-        r"\b\b",
-        r"\b\b",
-        r"\b\b",
-        r"\b\b",
-        r"\b\b",
-        r"\b\b",
-        r"\b\b",
-        r"\b\b",
-        r"\b\b",
-        r"\b\b",
-        r"\b\b",
-        r"\b\b",
-        r"\b\b",
-        r"\b\b",
-        r"\b\b",
-        r"\b\b",
-        r"\b\b",
-        r"\b\b",
-        r"\b\b",
-        r"\b\b",
-        r"\b\b",
-        r"\b\b",
-        r"\b\b",
-        r"\b\b",
-        r"\b\b",
-        r"\b\b",
-        r"\b\b",
-        r"\b\b",
-        r"\b\b",
-        r"\b\b",
-        r"\b\b",
-        r"\b\b",
-        r"\b\b",
-        r"\b\b",
-        r"\b\b",
-        r"\b\b",
-        r"\b\b",
-        r"\b\b",
-        r"\b\b",
-        r"\b\b",
-        r"\b\b",
-        r"\b\b",
-        r"\b\b",
-        r"\b\b",
-        r"\b\b",
-        r"\b\b",
-        r"\b\b",
-        r"\b\b",
-        r"\b\b",
-        r"\b\b",
-        r"\b\b",
-        r"\b\b",
-        r"\b\b",
-        r"\b\b",
-        r"\b\b",
-        r"\b\b",
-        r"\b\b",
-        r"\b\b",
-        r"\b\b",
-        r"\b\b",
-        r"Â",
-        r"~+",
-        r"\|+",
-        r"/+",
-        r"\{+",
-        r"\}+",
-        r"\[+",
-        r"\]+",
-        r"\(+",
-        r"\)+",
-        r";+",
-        r"->",
-        r">+",
-        r"<+",
-        r"=+",
-        r"'+",
-        r"`+",
-        r"\"+",
-        r"\++",
-        r"#+",
-        r"\d+:\d+",
-        r":{2,}",
-        r"\b\d+(\.\d+)+\.?\b",
-        r"\.{2,}",
-        r"\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}",
-        r"\d{2}:\d{2}:\d{2}(\.\d{3})?",
-        r"\d{4}-\d{2}-\d{2}",
-        r"-+"
+#         r"\b\b",
+#         r"\b\b",
+#         r"Â",
+#         r"~+",
+#         r"\|+",
+#         r"/+",
+#         r"\{+",
+#         r"\}+",
+#         r"\[+",
+#         r"\]+",
+#         r"\(+",
+#         r"\)+",
+#         r";+",
+#         r"->",
+#         r">+",
+#         r"<+",
+#         r"=+",
+#         r"'+",
+#         r"`+",
+#         r"\"+",
+#         r"\++",
+#         r"#+",
+#         r"\d+:\d+",
+#         r":{2,}",
+#         r"\b\d+(\.\d+)+\.?\b",
+#         r"\.{2,}",
+#         r"\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}",
+#         r"\d{2}:\d{2}:\d{2}(\.\d{3})?",
+#         r"\d{4}-\d{2}-\d{2}",
+#         r"-+"
     ]
     
     case_insensitive_elements_to_remove = [
-        r"\b\b",
-        r"\b",
-        r"\b\b",
-        r"\b\b",
-        r"\b"
+#         r"\b\b",
+#         r"\b"
     ]
     
-    def __init__(self, data_file, clean_brs=True, \
-    use_stemmer=True, use_lemmatizer= False, \
-    stop_words_removal=True, punctuation_removal=True, \
-    numbers_removal=True):
+    def __init__(self, data_file, clean_brs=True, use_stemmer=True, \
+                 use_lemmatizer= False, stop_words_removal=True, \
+                 punctuation_removal=True, numbers_removal=True):
         """Constructor"""
         self.content = None # Will contain the content of the file 
-        # with all BRs
+        # with all the bug reports
         self.html_parser = None # Will contain an instance of a HTML 
         # parser
         self.stemmer = None # Will contain the stemmer which will be 
@@ -158,7 +79,8 @@ class DataPreProcesser:
         self.output = [] # Will contain the pre-processed data to dump 
         self._current_dir = os.path.dirname(os.path.abspath( \
         inspect.getfile(inspect.currentframe())))
-        # Will contain the name of the file which contains the BRs
+        # Will contain the name of the file which contains the bug 
+        # reports
         self.data_file = os.path.join(self._current_dir, data_file) 
         if clean_brs:
             # Instantiation of a HTMLParser
@@ -180,6 +102,13 @@ class DataPreProcesser:
         if numbers_removal:
             self.case_sensitive_elements_to_remove.append(r"\b\d+\b")
 
+    def _load_json_file(self):
+        """Opens and loads the data from a JSON file."""
+        print("Loading...") # Debug
+        with open(self.data_file) as json_data:
+            self.content = json.load(json_data)
+        print("Loaded") # Debug
+
     def clean_data(self):
         """Method to clean the data
 
@@ -188,29 +117,29 @@ class DataPreProcesser:
         print("BR number:") # Debug
         i = 0  
         # Below, we open the data file
-        with open(self.data_file, encoding="utf8") as f:
-            self.content = csv.DictReader(f, delimiter=";")
-            for row in self.content:
-                # print(row["W"]) # Debug
-                i += 1
-                print(i)
-                # Then, we clean them and add them to a list
-                self.output.append({
-                    "br_id": self \
-                    .__clean_br_id(row["X"]),
-                    "observation": self \
-                    .__clean_observation( \
-                        row["Y"] \
-                        ),
-                    "heading": self \
-                    .__clean_heading(row["Z"]),
-                    "assignee": self \
-                    .__clean_assignee( \
-                        row["A"] \
-                        )
-                })
-                # if i == 1000:
-                #     break
+        self._load_json_file()        
+        for br in self.content:
+            # print(row["W"]) # Debug
+            i += 1
+            print(br)
+            print(i)
+            # Then, we clean them and add them to a list
+            self.output.append({
+                "bug_id": self \
+                ._clean_bug_id(br["bug_id"]),
+                "description": self \
+                ._clean_description( \
+                    br["description"] \
+                    ),
+                "title": self \
+                ._clean_title(br["title"]),
+                "assigned_to": self \
+                ._clean_assigned_to( \
+                    br["assigned_to"] \
+                    )
+            })
+            if i == 1000:
+                break
 
     def write_data(self, data_file):
         """Method to write the data
@@ -219,9 +148,9 @@ class DataPreProcesser:
         parameter.
         """
         with open(data_file, 'w') as output_file:
-            json.dump(self.output, output_file)
+            json.dump(self.output, output_file, indent=4)
             
-    def __clean_string(self, string_to_clean):
+    def _clean_string(self, string_to_clean):
         """Method to clean a given string"""
         if string_to_clean is None:
             string_to_clean = ""
@@ -229,8 +158,8 @@ class DataPreProcesser:
             if self.html_parser is not None:
                 # We should clean the string
                 string_to_clean = self \
-                .__escape_html_char_and_remove_space(string_to_clean)
-                string_to_clean = self.__remove_url(string_to_clean)
+                ._escape_html_char_and_remove_space(string_to_clean)
+                string_to_clean = self._remove_url(string_to_clean)
                 for el_to_remove in self \
                 .case_sensitive_elements_to_remove:
                     string_to_clean = re.sub(el_to_remove, " ", \
@@ -240,8 +169,10 @@ class DataPreProcesser:
                     string_to_clean = re.sub(el_to_remove, " ", \
                     string_to_clean, flags=re.I)
                 string_to_clean = self \
-                .__replace_newline_char_and_strip(string_to_clean)
-                re.sub(r"\s+", " ", string_to_clean)
+                ._replace_newline_char_and_strip(string_to_clean)
+                string_to_clean = re.sub(r"\s+", " ", string_to_clean)
+                string_to_clean = re.sub(r"\r+", " ", string_to_clean)
+                string_to_clean = re.sub(r"\t+", " ", string_to_clean)
         tokens = word_tokenize(string_to_clean)
         if self.stemmer is not None:
             # We should stem the string to clean
@@ -261,20 +192,31 @@ class DataPreProcesser:
             if not all(char in string.punctuation for char in token)]
         return tokens
 
-    def __clean_heading(self, heading):
+    def _clean_title(self, title):
         """Method to clean a given heading"""
-        return self.__clean_string(heading)
+        return self._clean_string(title)
 
-    def __clean_observation(self, observation):
+    def _clean_description(self, observation):
         """Method to clean a given observation"""
         # Below, we clean, tokenize and stem the observation field
-        return self.__clean_string(observation)
+        return self._clean_string(observation)
 
-    def __clean_br_id(self, br_id):
-        """Method to clean a given BR-id"""
-        return self.__escape_html_char_and_remove_space(br_id)
+    def _clean_bug_id(self, bug_id):
+        """Method to clean a given bug_id"""
+        bug_id = self._escape_html_char(bug_id)
+#         print(bug_id) # Debug
+        return bug_id.replace(u"Bug\u00a0", " ")
 
-    def __escape_html_char_and_remove_space(self, data):
+    def _escape_html_char(self, data):
+        """Method to escape HTML chars and remove spaces"""
+        data = "" if data is None else data
+#         print(data) # Debug
+        if self.html_parser is not None:
+            return self.html_parser.unescape(data)
+        else:
+            return data
+
+    def _escape_html_char_and_remove_space(self, data):
         """Method to escape HTML chars and remove spaces"""
         data = "" if data is None else data
         if self.html_parser is not None:
@@ -283,19 +225,19 @@ class DataPreProcesser:
         else:
             return data
 
-    def __clean_assignee(self, assignee):
-        """Method to clean an assignee"""
-        return self.__escape_html_char_and_remove_space(assignee)
+    def _clean_assigned_to(self, assigned_to):
+        """Method to clean an assigned_to field"""
+        return self._escape_html_char_and_remove_space(assigned_to)
 
-    def __replace_newline_char_and_strip(self, data):
+    def _replace_newline_char_and_strip(self, data):
         """Method to replace a '\n' char and to strip a string"""
-        return self.__replace_newline_char_by_space(data).strip()
+        return self._replace_newline_char_by_space(data).strip()
 
-    def __replace_newline_char_by_space(self, data):
+    def _replace_newline_char_by_space(self, data):
         """Method to replace a '\n'"""
         return data.replace("\n", " ")
 
-    def __remove_url(self, data):
+    def _remove_url(self, data):
         """Method to remove any URL"""
         data = re \
         .sub(r'(https|http|file)?:\/\/\/?(\/|\w|\.|\?|\=|\&|\%|\-)*\b' \
@@ -315,13 +257,13 @@ class DataPreProcesser:
         else:
             return wordnet.NOUN
 
-if __name__ == "__main__":
+def main():
     """Main program"""
     maxInt = sys.maxsize
     decrement = True
     while decrement:
-        # Decrease the maxInt value by factor 10 
-        # as long as the OverflowError occurs.
+        # Decrease the maxInt value by factor 10 as long as the 
+        # OverflowError occurs.
         decrement = False
         try:
             csv.field_size_limit(maxInt)
@@ -339,8 +281,8 @@ if __name__ == "__main__":
     punctuation_removal = True
     numbers_removal = True
 
-    data_file = "X.csv" # Path of the file containing 
-    # the data
+    data_file = "./eclipse_jdt/scrap_eclipse_jdt/sorted_brs.json" 
+    # Path of the file containing the data
     
     # Below, we are giving a relevant name to the output file
     clean_brs_string = "" if clean_brs else "out" 
@@ -367,4 +309,7 @@ if __name__ == "__main__":
     data_pre_processer.write_data(output_data_file) # We write the 
     # data into the given output file
     
-    print("--- {} seconds ---".format(time.time() - start_time)
+    print("--- {} seconds ---".format(time.time() - start_time))
+
+if __name__ == "__main__":
+    main()
