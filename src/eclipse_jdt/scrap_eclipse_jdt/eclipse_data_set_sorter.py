2a4867d8a95f31e7494e9f150fc1e18fa6698cff
Daniel Artchounin <artchounindaniel@gmail.com>
2017-09-11 23:55:33 +0200

    Fixed some bugs related to two import statements

--- src/eclipse_jdt/scrap_eclipse_jdt/eclipse_data_set_sorter.py
+++ src/eclipse_jdt/scrap_eclipse_jdt/eclipse_data_set_sorter.py
@@ -19,7 +19,7 @@ inspect.getfile(inspect.currentframe())))
 parent_dir = os.path.dirname(current_dir)
 grand_parent_dir = os.path.dirname(parent_dir)
 os.sys.path.insert(0, grand_parent_dir)
-from scrap.data_set_sorter import DataSetSorter
+from sort.data_set_sorter import DataSetSorter
 
 def main():
 	current_dir = os.path.dirname(os.path.abspath( \
